1c2749f219625c17dc014709b559cd65f90dac60
keerthanjoshua <81298333+keerthanjoshua@users.noreply.github.com>
2021-05-07 21:18:47 +0530

    Add files via upload

---
+++ Block.js
@@ -0,0 +1,22 @@
+class Block{
+    constructor(x,y,width,height){
+        var options = {
+            'restitution':0.2,
+            'friction':5
+        }
+        this.body = Bodies.rectangle(x,y,width,height,options);
+  this.width=width;
+  this.height=height;
+  World.add(world,this.body);
+    }
+    display(){
+        push();
+        //translate(this.body.position.x, this.body.position.y);
+        var pos = this.body.position;
+        rectMode(CENTER);
+        fill(rgb(3,252,194));
+        rect(pos.x,pos.y,this.width,this.height);
+
+        pop();
+    }
+}
